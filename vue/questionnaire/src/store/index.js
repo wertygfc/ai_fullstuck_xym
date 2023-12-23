97506fdbf3ddb21db4a7c114f59def5831c9b0ce
wertygfc <2207141937@qq.com>
2023-12-23 17:05:38 +0800

    questionnaire

--- vue/questionnaire/src/store/index.js
+++ vue/questionnaire/src/store/index.js
@@ -1,13 +1,50 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
-  state: {
+  state: {  // == data
+    questionList: [],
+    itemNum: 1, // 初始
+    answerId: []  // 用来存最终答案的
   },
-  getters: {
+  getters: {  // == computed
+    rightAnswer(state) {
+      let arr = []
+      for(let question of state.questionList){
+        for(let answer of question.topic_answer) {
+          if(answer.is_standard_answer == 1){
+            arr.push(answer.topic_answer_id)
+          }
+        }
+      }
+      return arr
+    }
   },
-  mutations: {
+  mutations: {  // == methods
+    getQuestionList(state, list) {  // 所有写在mutation中的当函数的第一个参数都是state
+      state.questionList = list
+    },
+    nextItem(state) {
+      state.itemNum += 1
+    },
+    saveAnswer(state, id) {
+      state.answerId.push(id)
+    }
   },
-  actions: {
+  actions: {  // == methods  async
+    getQuestionListAction(context) {
+      axios.get('https://www.fastmock.site/mock/3a33c631bcba043a378b4ceafbf994b8/question/questions')
+      .then(res => {
+        console.log(res);
+        context.commit('getQuestionList', res.data)   // 触发mutation中的方法
+      })
+    },
+    nextItemAction({ commit, state }, id) {
+      if(state.itemNum < state.questionList.length) {
+        commit('nextItem')
+      }
+      commit('saveAnswer', id)
+    }
   },
   modules: {
   }
--- vue/questionnaire/src/views/item/index.vue
+++ vue/questionnaire/src/views/item/index.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
-        item
+        <ItemContainer parentComponent="item" />
     </div>
 </template>
 
 <script>
+import ItemContainer from '@/components/itemContainer.vue';
+import { mapActions } from 'vuex';
     export default {
-        
+        components: {
+            ItemContainer
+        },
+        created() {  // 生命周期函数：在页面加载过程中自动触发
+            this.getQuestionListAction()
+        },
+        methods: {
+            ...mapActions(['getQuestionListAction'])
+        }
     }
 </script>
 
