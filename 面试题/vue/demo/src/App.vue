fe77ac1688451e2c20499bbb4418e3a7f1f8faf7
wertygfc <2207141937@qq.com>
2024-03-25 21:36:00 +0800

    vue面试题

---
+++ 面试题/vue/demo/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hd">
+    <input type="text" v-model="msg">
+    <button @click="add">添加</button>
+  </div>
+
+  <Child  :list="list" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Child from './components/Child.vue'
+const message = ref('')
+const msg = ref('')
+
+const add = () => {
+  MessageChannel.value = message
+}
+
+</script>
+
+<style lang="css" scoped>
+.hd input{
+  width: 200px;
+  padding: 10px 0;
+  font-size: 20px;
+}
+.hd button{
+  padding: 10px 20px;
+  font-size: 20px;
+}
+</style>
---
+++ 面试题/vue/demo/src/components/Child.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bd">
+      <ul>
+        <li v-for="item in list">{{item}}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script setup>
+import { defineProps } from 'vue';
+
+const list = ref(['html', 'css'])
+
+  defineProps({
+    message:{
+        type: String,
+        default: () => []
+    }
+  })
+  </script>
+  
+  <style lang="css" scoped>
+  
+  </style>
